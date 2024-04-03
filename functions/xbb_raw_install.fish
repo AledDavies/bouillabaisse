e191441933888ef95ce41f185a6cc519dc5436b2
Aled Davies <awd@well.com>
2024-04-03 10:04:47 -0700

    Update xbb_raw_install for new package structure

--- functions/xbb_raw_install.fish
+++ functions/xbb_raw_install.fish
@@ -2,37 +2,33 @@ function xbb_raw_install
   set shell $argv[1]
   set package_name $argv[2]
 
+  function pkg_apply
+    if test -e "$argv[2]"
+      echo "⚙️  Applying $argv[2]"	  
+      eval "$argv[1] $argv[2]"
+    end
+  end	  
+
   if not type -t "$shell" > /dev/null
-    echo "ERROR: shell `$shell` cannot be run"
+    echo "🚨 ERROR: shell `$shell` cannot be run"
     return 1 
   end  
 
   set shell_path "$shell"
 
   if test ! -d "$package_name"	
-    echo "ERROR: Cannot find $package_name directory"
+    echo "🚨 ERROR: Cannot find $package_name directory"
     return 1
   end
 
-  echo "Installing $package_name"
+  echo "📦️ Installing $package_name"
 
-  if test -e "$package_name/preconditions-$S_OSNAME-$S_PLATFORM"
-    eval "$shell_path $package_name/preconditions-$S_OSNAME-$S_PLATFORM"
-  end
-
-  if test -e "$package_name/preconditions"
-    eval "$shell_path $package_name/preconditions"
-  end
-
-  if test -e "$package_name/install"
-    eval "$shell_path $package_name/install"
-  end
-
-  if test -e "$package_name/postconditions"
-    eval "$shell_path $package_name/postconditions"
-  end
-
-  if test -e "$package_name/postconditions-$S_OSNAME-$S_PLATFORM"
-    eval "$shell_path $package_name/postconditions-$S_OSNAME-$S_PLATFORM"
-  end
+  pkg_apply "$shell_path" "$package_name/preconditions-$S_OSNAME-$S_PLATFORM"
+  pkg_apply "$shell_path" "$package_name/preconditions"
+  pkg_apply "$shell_path" "$package_name/install-$S_OSNAME-$S_PLATFORM"
+  pkg_apply "$shell_path" "$package_name/install"
+  pkg_apply "$shell_path" "$package_name/configure-$S_OSNAME-$S_PLATFORM"
+  pkg_apply "$shell_path" "$package_name/configure"
+  pkg_apply "$shell_path" "$package_name/postconditions-$S_OSNAME-$S_PLATFORM"
+  pkg_apply "$shell_path" "$package_name/postconditions"
 end
